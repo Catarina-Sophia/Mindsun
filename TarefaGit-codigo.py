1953843cc58e96a593f76a5f439735b986e73584
Catarina-Sophia <catarina.braga@cear.ufpb.br>
2020-05-15 21:01:34 -0300

    Arquivo de valores aleatorios adicionado

---
+++ TarefaGit-codigo.py
@@ -0,0 +1,3 @@
+import random
+for i in range(13):
+	print(random.randrange(0,51))
--- TarefaGit.py
+++ TarefaGit.py
@@ -1,3 +1,18 @@
-import random
-for i in range(13):
-	print(random.randrange(0,51))
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\catar\OneDrive\Documentos\PROBEX - Mindsun\GIT\GIT\Mindsun\TarefaGit-codigo.py
+48
+50
+13
+21
+4
+35
+44
+29
+33
+41
+12
+9
+6
+>>> 
